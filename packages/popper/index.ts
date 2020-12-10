d1ab5fc49eea8f8ea1b2c9a259abae7114ca687e
zouhang <zouhang@didiglobal.com>
2020-12-10 21:35:49 +0800

    fix: fix some components import type

--- packages/popper/index.ts
+++ packages/popper/index.ts
@@ -10,4 +10,5 @@ export default Popper
 export { default as defaultProps, Effect } from './src/use-popper/defaults'
 export type { TriggerType, IPopperOptions } from './src/use-popper/defaults'
 export { default as usePopper } from './src/use-popper/index'
+export { Options, Placement } from './src/use-popper/defaults'
 export * from './src/renderers/index'
